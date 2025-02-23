759a69fa5b87786ca9b190d54acc7a811007f54b
Unconvie <nigihayamikohakunushi519@gmail.com>
2025-02-23 20:21:17 +0800

    core oop WinAPI

---
+++ oopWin.cpp
@@ -0,0 +1,79 @@
+#include "oopWin.h"
+#include "positioner.h"
+
+template<class T>
+void deleteSafely(T*& ptr) {
+    T* buff = ptr;
+    ptr = 0;
+    delete buff;
+}
+
+
+HINSTANCE instance = 0;
+std::set<std::string> isRegistry = std::set<std::string>();
+
+void BoolApp::init(HINSTANCE ainstance)
+{
+	instance = ainstance;
+}
+
+LRESULT BoolApp::wndProc(HWND ahwnd, UINT message, WPARAM wparam, LPARAM lparam)
+{
+	switch (message){
+		case WM_NCCREATE:{
+			SetWindowLongPtr(ahwnd, 0, 0);
+			return DefWindowProc(ahwnd, message, wparam, lparam);
+		}
+		case WM_NCDESTROY:{ //конец удаления
+			HWND parentHWND = GetParent(ahwnd);
+			if(parentHWND){
+				InvalidateRect (parentHWND, 0, 0);
+			}
+		}
+		case WM_DESTROY:{ //начало удаления
+			ProcessView *ptr = GetWindowLongPtr(ahwnd, 0);
+			if(ptr){
+				SetWindowLongPtr(ahwnd, 0, 0);
+				ptr->hwnd = 0;
+				delete ptr;
+			}
+			return DefWindowProc(ahwnd, message, wparam, lparam);
+		}
+		case WM_SIZE:{
+			ProcessView *ptr = GetWindowLongPtr(ahwnd, 0);
+			if(!ptr){
+				return DefWindowProc(ahwnd, message, wparam, lparam);
+			}
+			if(GetWindowLong(ahwnd, GWL_STYLE) & WS_SIZEBOX){
+				ptr->size.height = HIWORD(lparam);
+				ptr->size.width = LOWORD(lparam);
+			}
+			
+		}
+		case WM_MOVE:{
+			ProcessView *ptr = GetWindowLongPtr(ahwnd, 0);
+			if(!ptr){
+				return DefWindowProc(ahwnd, message, wparam, lparam);
+			}
+			Positioning(ptr);
+		}
+		default:{
+			ProcessView *ptr = GetWindowLongPtr(ahwnd, 0);
+			if(!ptr){
+				return DefWindowProc(ahwnd, message, wparam, lparam);
+			}
+			ptr->view->wndProc(ahwnd, message, wparam, lparam, ptr);
+		}
+	}
+}
+
+void BoolApp::View::enable()
+{
+	enabled = true;
+}
+
+void BoolApp::View::disable()
+{
+	enabled = false;
+	deleteSafely(PV);
+}
--- oopWin.h
+++ oopWin.h
@@ -10,11 +10,45 @@ namespace BoolApp{
 	class View;
 	class ProcessView;
 	void init(HINSTANCE ainstance);
+	LRESULT wndProc(HWND ahwnd, UINT message, WPARAM wparam, LPARAM lparam);
+	class pcomposite;
+	class pcomponent;
+	class composite;
+	class component;
+	
 	class View{
 		public:
 		ProcessView *PV = 0;
 		virtual std::string getSzWindowClass() = 0;
-		virtual void Register() = 0;
+		virtual void Register(WNDCLASSA&){};
+		virtual LRESULT wndProc(HWND ahwnd, UINT message, WPARAM wparam, LPARAM lparam, ProcessView *ptr){};
+
+		void sregister(){
+			WNDCLASSA wca = {0};
+			//адрес ф-ции обработки сообщений
+			wca.lpfnWndProc = (WNDPROC)BoolApp::wndProc;
+			//стиль окна
+			wca.style = CS_HREDRAW|CS_VREDRAW;
+			//дискриптор экземпляра приложения
+			//название класса
+			wca.hInstance = instance;
+			wca.lpszClassName = getSzWindowClass().c_str();
+			//загрузка курсора
+			wca.hCursor = LoadCursor(NULL, IDC_ARROW);
+			//загрузка цвета окон
+			wca.hbrBackground = (HBRUSH)COLOR_APPWORKSPACE;
+			//резерв места для указателя на PV
+			wca.cbWndExtra = sizeof(void*);
+			Register(wca);
+			//регистрация класса
+			RegisterClassA(&wca);
+		}
+
+		~View(){
+			PV->view = 0;
+			disable();
+		}
+
 		View(){
 
 		}
@@ -24,11 +58,11 @@ namespace BoolApp{
 
 		private:
 		bool enabled = false;
-
 	};
 
 	class ProcessView{
 		public:
+		HWND hwnd;
 		View *view;
 		Size size = Size(0,0);
 		Margin margin = Margin(0, 0, 0, 0);
@@ -37,15 +71,25 @@ namespace BoolApp{
 			hwnd = ahwnd;
 
 			if ( !isRegistry.contains ( aview->getSzWindowClass () ) ) {
-        aview->Register ();
+        aview->sregister ();
         isRegistry.insert ( aview->getSzWindowClass () );
       }
 
 		}
+		~ProcessView(){
+			if(view){
+				view->PV = 0;
+				view = 0;
+			}
+			
+			if(hwnd){
+				SetWindowLongPtr(hwnd, 0, 0);
+				SendMessage(hwnd, WM_CLOSE, 0, 0);
+			}
+		}
 
 		virtual void construction() = 0;
 		private:
-		HWND hwnd;
 	};
 
 
