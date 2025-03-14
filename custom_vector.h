d754179201a4fd194a48560ff0ea48461a115b3b
paprika8 <aserstritt@gmail.com>
2025-03-14 21:38:57 +0800

    custom_vector fix remove

--- custom_vector.h
+++ custom_vector.h
@@ -13,16 +13,17 @@ struct custom_vector{
 		data[size++] = a;
 	}
 
-	void remove(T &a){
+	bool remove(T &a){
 		for(T* h = data; h < h + size; h++){
 			if(*h == a){
 				if(h - data != size - 1)
 					memmove(h, h + 1, (size - (h - data) - 1) * sizeof(T));
 				data = realloc(data, size * sizeof(T) - sizeof(T));
 				--size;
-				break;
+				return true;
 			}
 		}
+		return false;
 	}
 
 	T& operator[](int i){
@@ -32,6 +33,63 @@ struct custom_vector{
 	~custom_vector(){
 		free(data);
 	}
+
+	iterator begin(){
+		return iterator(data);
+	}
+	iterator end(){
+		return iterator(data + size);
+	}
+
+	struct iterator{
+		iterator(T* ait){
+			it = ait;
+		}
+
+		T& operator*(){
+			return *it;
+		}
+
+		iterator operator+=(int a){
+			it += a;
+			return *this;
+		}
+		iterator operator+(int a){
+			iterator buf(it);
+			buf += a;
+			return buf;
+		}
+
+		iterator operator-=(int a){
+			it -= a;
+			return *this;
+		}
+		iterator operator-(int a){
+			iterator buf(it);
+			buf -= a;
+			return buf;
+		}
+
+		int operator-(iterator b){
+			return it - b.it;
+		}
+
+		bool operator==(iterator b){
+			return it == b.it;
+		}
+		bool operator!=iterator b){
+			return it != b.it;
+		}
+		bool operator<(iterator b){
+			return it < b.it;
+		}
+		bool operator>(iterator b){
+			return it > b.it;
+		}
+		private:
+		T* it;
+	}
+
 private:
 	T* data;
 	size_t size;
