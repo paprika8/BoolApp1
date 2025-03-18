23149e6cd1d438b8d6d092f4929f43263d1d9a5a
JaRiex11 <150585653+JaRiex11@users.noreply.github.com>
2025-03-18 13:27:24 +0800

    task_4 (очистил от лишнего)

--- tasks/task_4/task_4.cpp
+++ tasks/task_4/task_4.cpp
@@ -1,89 +1,11 @@
 #include "..\..\terms\term.hpp"
 #include <iostream>
-#include <string>
 #include <time.h>
 #include <vector>
 #include <random>
 #include <chrono>
 
 using namespace std;
-
-int countDigits(int number)
-{
-    if (number == 0)
-    {
-        return 1; // Число 0 имеет одну цифру
-    }
-
-    int count = 0;
-    while (number != 0)
-    {
-        number /= 10; // Убираем последнюю цифру
-        count++;
-    }
-    return count;
-}
-
-int twoInPower(int power)
-{
-    if (power == 0)
-        return 1;
-
-    int res = 2;
-    while (power > 1)
-    {
-        res <<= 1;
-        power--;
-    }
-    return res;
-}
-
-int getPowerOfTwo(size_t n)
-{
-    if (n == 0)
-    {
-        return -1;
-    }
-
-    int power = 0;
-    while (n > 1)
-    {
-        n >>= 1;
-        power++;
-    }
-
-    return power;
-}
-
-void log_table(vector<bool> &v)
-{
-    int n = getPowerOfTwo(v.size());
-
-    cout << '|';
-    for (int i = 1; i <= n; i++)
-    {
-        cout << " x" << i << " |";
-    }
-    cout << " f |" << '\n';
-    for (int i = 0; i < v.size(); i++)
-    {
-        cout << '|';
-
-        for (int j = n - 1; j >= 0; j--)
-        {
-            int offs = 1 << j;
-            bool t = i & offs;
-
-            for (int k = 0; k < countDigits(n - j) + 1; k++)
-            {
-                cout << ' ';
-            }
-            cout << t << " |";
-        }
-
-        cout << ' ' << v[i] << " |" << '\n';
-    }
-}
 
 int main()
 {
