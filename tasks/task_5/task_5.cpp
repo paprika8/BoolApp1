9e4ed88eb6fc7047e4fb158b96faca26c73ad012
JaRiex11 <150585653+JaRiex11@users.noreply.github.com>
2025-03-20 22:21:45 +0800

    task_5, недоделаный. Идея норм, реалезация хромает

--- tasks/task_5/task_5.cpp
+++ tasks/task_5/task_5.cpp
@@ -61,154 +61,119 @@ int main()
 
     uniform_int_distribution<> dis_x(2, 3); // количество переменных
     int amt_x = dis_x(gen);
+    // Для отладки:
+    amt_x = 4;
+    //
     cout << "Amt of vars: " << amt_x << endl;
     uniform_int_distribution<> dis_of_dum(0, amt_x); // количество фиктивных переменных
     int amt_dum = dis_of_dum(gen);
+    // Для отладки:
+    amt_dum = 2;
+    //
     cout << "Amt of dum vars: " << amt_dum << endl;
     uniform_int_distribution<> num_of_x(1, amt_x); // рандомайзер для номера переменной
     uniform_int_distribution<> z_or_o(0, 1);       // рандомайзер для 0 и 1
 
-    vector<pair<bool, bool>> vars(amt_x + 1, pair(0, 1)); // вектор, фиксирующий фиктивность переменной
-    vars[0].first = 1;
-    vars[0].second = 0;
+    // Так как это часто используется, заведём переменную
+    int two_in_pow_sig_x = twoInPower(amt_x - amt_dum);
+
+    uniform_int_distribution<> name_of_fun(1, two_in_pow_sig_x - 2); // рандомайзер номера имени функции
+    int num_name_f = name_of_fun(gen);
+
+    // для отладки
+    num_name_f = 11;
+
+    vector<bool> vf_buf(two_in_pow_sig_x); // Буферный вектор переменной только с существенными переменными
+    vector<bool> vf(twoInPower(amt_x));    // Полный, итоговый вектор переменной
+
+    vector<bool> vars(amt_x + 1); // вектор, фиксирующий фиктивность переменной (1 индексация)
+    // vars[0] = 1;
+    vars[2] = 1;
+    vars[4] = 1;
     // раставляем фиктивность переменных
-    for (int i = 0; i < amt_dum; i++)
+    /*for (int i = 0; i < amt_dum; i++)
     {
         while (true)
         {
             int ind_of_dum = num_of_x(gen);
-            if (!vars[ind_of_dum].first)
+            if (!vars[ind_of_dum])
             {
-                vars[ind_of_dum].first = 1;
+                vars[ind_of_dum] = 1;
                 break;
             }
         }
-    }
-    // Работаем с существенными переменными
-    vector<pair<bool, bool>> vars_for_ans = vars;
+    }*/
 
-    int num_signif_x = vars.size() - 1;
-    while (num_signif_x > 0)
+    // Создали вектор использующий все существ переменые
+
+    for (int i = two_in_pow_sig_x - 1; i >= 0; --i)
     {
-        if (!vars[num_signif_x].first && vars[num_signif_x].second)
-        {
-            vars[num_signif_x].second = 0;
-            break;
-        }
-        num_signif_x--;
-    }
-    if (num_signif_x <= 0)
-    {
-        for (int i = 0; i < twoInPower(amt_x); i++)
-        {
-            cout << '0';
-        }
-        cout << endl;
-        return 0;
+        // Сдвигаем биты вправо и проверяем значение текущего бита
+        int bit = (num_name_f >> i) & 1;
+        vf_buf[(i + two_in_pow_sig_x) % two_in_pow_sig_x] = bit;
     }
 
-    vars = vars_for_ans;
-    // int len_ost_vec = twoInPower(amt_x - num_signif_x);
-    int len_ost_vec = twoInPower(amt_x - 1);
-    string ost1 = "";
-    for (int j = 0; j < len_ost_vec; j++)
-    {
-        ost1 += '0';
-    }
-    string ost2 = "";
-    for (int j = 0; j < len_ost_vec; j++)
-    {
-        ost2 += '1';
-    }
-
-    vector<bool> v = makeVecFromOstat(ost1, ost2, num_signif_x);
-    cout << "num_signif_x = " << num_signif_x << endl;
-    cout << "v size = " << v.size() << endl;
-
-    for (int j = 0; j < v.size(); j++)
-    {
-        cout << v[j] << ' ';
-    }
-    cout << endl;
-    num_signif_x--;
-    for (int i = 0; i < amt_x - amt_dum; i++)
-    {
-        // тут работаем с остальными существенными переменными
-        while (num_signif_x >= 0)
-        {
-            if (!vars[num_signif_x].first && vars[num_signif_x].second)
-            {
-                vars[num_signif_x].second = 0;
-                break;
-            }
-            num_signif_x--;
-        }
-        if (num_signif_x < 0)
-        {
-            break;
-        }
-        cout << "num_signif = " << num_signif_x << endl;
-
-        // Инвертируем случайное количество бит, на случайных местах
-        int start_pos = twoInPower(amt_x - num_signif_x);
-        uniform_int_distribution<> ind_x(0, v.size() - 1);
-        int pos = ind_x(gen);
-        cout << "pos = " << pos << endl;
-        v[pos] = !(v[pos]);
-        for (int j = 0; j < v.size(); j++)
-        {
-            cout << v[j] << ' ';
-        }
-        cout << endl;
-    }
-
-    // Обработаем вектор так, чтобы фиктивные остались фиктивными
-    for (int i = 0; i < amt_dum; i++)
-    {
-        int num_dum_x = amt_x;
-        while (num_dum_x > 0)
-        {
-            if (vars[num_dum_x].first)
-            {
-                vars[num_dum_x].first = 0;
-                break;
-            }
-            num_dum_x--;
-        }
-        cout << "num_dum_x = "
-             << num_dum_x << "\n\n";
-
-        int step = twoInPower(num_dum_x);
-        int k = 0;
-        // Проверь шаг, что то с ним не так, придётся пересчитать
-        for (int i = 0; i < v.size(); i += step * 2)
-        {
-            cout << "Enter the cycle\n";
-            k = i;
-            for (int j = i + step * 2; j < v.size(); j++, k++)
-            {
-                cout << "Enter the second cycle\n";
-                v[k] = v[j];
-                cout << "k = " << k << "; j = " << j << endl;
-            }
-        }
-    }
-
-    // Описание алгоритма создания вектора:
-    // Идём от самых "дальних" фиктивных переменных, то есть от переменных с самым большим индексом
-    // И составляем для их 0 и 1 остаточных одинаковые векторы (чтобы они были фиктивными)
-    // Затем для последующих переменных копируем этот вектор, чтобы у них на 0 и 1 остаточных тоже были
-    // одинаковые значения
-    // механизм составления вектора понятен
-    // и механизм соединения двух остточных по номеру переменной в вектор уже готов (задание 3)
-
-    // Переработанный алгоритм:
-    // Идём от дальних существенных переменных.
-    // Делаем для них тоже самое,  нам нужно просто сформировать какой то вектор функции
-    // Так же для всех последующих нужно чуть чуть менять, но не так, чтобы прошлые становились фиктивными
-
-    for (auto el : v)
+    cout << "Vector buffer:\n";
+    for (auto el : vf_buf)
     {
         cout << el << ' ';
     }
+    cout << endl;
+
+    // расширим вектор фиктивными переменными
+
+    int t_amt_x = amt_x - amt_dum + 1; // Относительное общее число переменных
+    for (int k = 0; k < amt_dum; k++, t_amt_x++)
+    {
+        int num_dum_x = 1;
+        while (num_dum_x < vars.size())
+        {
+            if (vars[num_dum_x])
+            {
+                vars[num_dum_x] = 0;
+                break;
+            }
+            num_dum_x++;
+        }
+        // Для отладки:
+        cout << "Num_dum_x = " << num_dum_x << endl;
+        //
+
+        int step = twoInPower(t_amt_x - num_dum_x);
+        int ind_in_buf = 0;
+        // int counter = 0; // счётчик переходов для ind_in_buf
+        for (int i = 0; i < twoInPower(t_amt_x); i += step * 2)
+        {
+            for (int j = i; j < i + step; j++)
+            {
+                vf[j] = vf_buf[ind_in_buf];
+                vf[j + step] = vf[ind_in_buf];
+                cout << "j = " << j << "  ";
+                cout << "ind_in_buf = " << ind_in_buf << '\n';
+                cout << "j + step = " << j + step << "  ";
+                cout << "ind_in_buf = " << ind_in_buf << '\n';
+                ind_in_buf++;
+                /*counter++;
+                if (counter >= 2)
+                {
+                    ind_in_buf++;
+                    counter = 0;
+                }*/
+            }
+        }
+        vf_buf = vf;
+        cout << "Result vector:\n";
+        for (auto el : vf)
+        {
+            cout << el << ' ';
+        }
+        cout << endl;
+    }
+
+    /*cout << "Result vector:\n";
+    for (auto el : vf)
+    {
+        cout << el << ' ';
+    }
+    cout << endl;*/
 }
